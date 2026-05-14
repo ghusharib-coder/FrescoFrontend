87fd1529f97f2ea0494912a18ff423a5ee527def
ghusharib-coder <ghusharibs@gmail.com>
2026-05-14 22:46:28 +0500

    fresco

--- src/Shoping-cart/ShopingCart.jsx
+++ src/Shoping-cart/ShopingCart.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 
 const ShopingCart = () => {
   return (
-    <div className='Shoping-cart'>
+    <div className='Shoping-cart' style={{width:'100vw'}}>
       <Navbar/>
       <Outlet/>
       <Footer/>
